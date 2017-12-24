f22497b41b26162817af86a7519d8e82563e6cd2
elbrag <ellenbrage@outlook.com>
2017-12-24 21:43:58 +0100

    lang buttons

--- js/scripts.js
+++ js/scripts.js
@@ -14,13 +14,42 @@ function openMenu(x){
 
 };
 
+// 
+// var checkinput = document.querySelector('.checkinput');
+// var eng_lang = document.getElementById('en');
+// var sve_lang = document.getElementById('sv');
+//
+//
+// $("input[type=checkbox]").change(function() {
+//
+//     if (checkinput.checked) {
+//       sve_lang.click();
+//     } else {
+//       eng_lang.click();
+//     }
+//
+// });
 
-var checkinput = document.querySelector('.checkinput');
-var eng_lang = document.querySelector('.lang-item-en');
-var sve_lang = document.querySelector('.lang-item-sv').children;
 
-function checklang() {
-  if (checkinput.checked) {
-    sve_lang.click();
-  }
-}
+// function checklang(x) {
+//
+//
+
+
+  // var sve = false;
+  // var eng = true;
+  //
+  // if (checkinput.checked) {
+  //
+  //     if (eng = true) {
+  //       sve_lang.click();
+  //       sve = true;
+  //     }
+  //
+  //     if (sve = true) {
+  //       eng_lang.click();
+  //       eng = true;
+  //     }
+  //   }
+
+  // }
--- partials/sitehead.php
+++ partials/sitehead.php
@@ -8,22 +8,27 @@
 
         <?php wp_nav_menu( array('menu' => 'Menu', 'container' => '', 'items_wrap' => '<ul class="main-ul">%3$s</ul>' )); ?>
 
-        <label class="switch">
-          <input class='checkinput' type="checkbox" onclick="checklang()">
+        <!-- <label class="switch">
+          <input class='checkinput' type="checkbox">
           <span class="checkslider"></span>
-        </label>
-        <ul class='lang-ul'>
+        </label> -->
 
+<div class='lang_items'>
           <?php $languages = pll_the_languages(array('raw'=>1));
           foreach($languages as $lang) {
             ?>
-            <li><a href='<?php echo $lang[url]; ?>' id='<?php echo $lang[slug]; ?>'></a></li>
+
+              <a href='<?php echo $lang[url]; ?>' id='<?php echo $lang[slug]; ?>'>
+                <button class='lang_item'>
+                    <?php echo $lang[slug]; ?>
+                </button>
+              </a>
+
             <?php
           }
-
           ?>
+</div>
 
-        </ul>
 <!-- pll_the_languages(array('raw'=>1)); -->
       </div>
 
