09406a43ae04ae8330bd5148ee85c865f5bc81ef
elbrag <ellenbrage@outlook.com>
2017-12-26 13:41:03 +0100

    fix

--- about.php
+++ about.php
@@ -64,7 +64,26 @@ if( have_posts() ) {
 
 <!--Section 3-->
 
+<section>
 
+    <h2><?php the_field('heading_3'); ?></h2>
+
+    <?php
+    $image3 = get_field('image_3');
+
+    $sizedimage3 = $image3['sizes'][ $singlelarge ];
+    $width = $image3['sizes'][ $singlelarge . '-width' ];
+    $height = $image3['sizes'][ $singlelarge . '-height' ];
+
+    if ($image3) {
+      ?>
+     <div class='image' style='background-image: url("<?php echo $sizedimage3;?>");'></div>
+     <?php }
+    ?>
+
+    <p><?php the_field('text_3');?></p>
+
+</section>
 
    </div><!--end of page specific tag-->
     <?php
