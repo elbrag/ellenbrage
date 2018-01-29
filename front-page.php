3cd74528020eaef3c9e15d97ad9bcba89cad2adf
elbrag <ellenbrage@outlook.com>
2018-01-29 15:37:21 +0100

    prfff

--- front-page.php
+++ front-page.php
@@ -14,14 +14,47 @@ if( have_posts() ) {
      the_post();
 
      ?>
+
+  <div id='front_background'>
+
   <div id='frontpage_text'>
 
-    <h1>Ellen Brage</h1>
-    <h2>Graphic designer</h2>
-    <h3>Web developer</h3>
-    <h4>Illustrator</h4>
+    <div class='header1_box'>
+        <!-- <h1 class='stroke'>Ellen Brage</h1> -->
+        <h1 class='fill'>Ellen Brage</h1>
+        <h1 class='fill_2'>Ellen Brage</h1>
+        <h1 class='fill_3'>Ellen Brage</h1>
+    </div>
+
+    <!-- <h2 class='stroke'>Graphic designer</h2>
+    <h3 class='stroke'>Web developer</h3>
+    <h4 class='stroke'>Illustrator</h4> -->
+
+    <p class='title'>
+      <span>G</span>
+      <span>r</span>
+      <span>a</span>
+      <span>p</span>
+      <span>h</span>
+      <span>i</span>
+      <span>c</span>
+
+      <span>d</span>
+      <span>e</span>
+      <span>s</span>
+      <span>i</span>
+      <span>g</span>
+      <span>n</span>
+      <span>e</span>
+      <span>r</span>
+    </p>
+    <p class='title'>Web developer</p>
+    <p class='title'>Illustrator</p>
 
   </div>
+
+
+</div>
   <?php
     }
   }
