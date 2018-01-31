bb0e8d9da6ec0d795fc9e6614b79f4b707bbd741
elbrag <ellenbrage@outlook.com>
2018-01-31 16:07:45 +0100

    d

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
   //theme slider area---------------------//
 
   $themeList = array("theme_1", "theme_2", "theme_3", "theme_4", "theme_5");
-  $themeNames = array("Swiss", "Crystal", "Pint", "Coral", "Steel");
+  $themeNames = array("Swiss", "Hoarfrost", "Funked", "Demure", "Chrysler");
 
 
   if (isset($_COOKIE['theme'])) {
