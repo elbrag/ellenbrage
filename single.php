b1d28e2ad5d6b172b2f3b5cf5c944d746937275f
elbrag <32334193+elbrag@users.noreply.github.com>
2018-11-10 00:29:46 +0100

    fixx

--- single.php
+++ single.php
@@ -190,36 +190,40 @@ if( have_posts() ) {
 
          </div><!--end of page specific tag-->
 
-         <section id='singleimages'>
-           <?php
+         <?php
+         $images = acf_photo_gallery('single_imgs', $post->ID);
+
+          if ($images) {
+            ?>
+            <section id='singleimages'>
+              <?php
 
 
-           $images = acf_photo_gallery('single_imgs', $post->ID);
+              if( count($images) ) {
+                foreach($images as $image){
+                  $id = $image['id']; // The attachment id of the media
+                  $title = $image['title']; //The title
+                  $caption= $image['caption']; //The caption
+                  $full_image_url= $image['full_image_url']; //Full size image url
+                  $thumbnail_image_url= $image['thumbnail_image_url']; //Get the thumbnail size image url 150px by 150px
+                  $url= $image['url']; //Goto any link when clicked
+                  $target= $image['target']; //Open normal or new tab
+                  $alt = get_field('photo_gallery_alt', $id); //Get the alt which is a extra field (See below how to add extra fields)
+                  $class = get_field('photo_gallery_class', $id); //Get the class which is a extra field (See below how to add extra fields)
+                  ?>
+                  <a class='gall_img' href='<?php echo $full_image_url ?>'><img class='single_img' src='<?php echo  $full_image_url ?>' /></a>
+                  <?php
+                }
+              }
 
-           if( count($images) ) {
-             foreach($images as $image){
-               $id = $image['id']; // The attachment id of the media
-               $title = $image['title']; //The title
-               $caption= $image['caption']; //The caption
-               $full_image_url= $image['full_image_url']; //Full size image url
-               $thumbnail_image_url= $image['thumbnail_image_url']; //Get the thumbnail size image url 150px by 150px
-               $url= $image['url']; //Goto any link when clicked
-               $target= $image['target']; //Open normal or new tab
-               $alt = get_field('photo_gallery_alt', $id); //Get the alt which is a extra field (See below how to add extra fields)
-               $class = get_field('photo_gallery_class', $id); //Get the class which is a extra field (See below how to add extra fields)
-               ?>
-               <a class='gall_img' href='<?php echo $full_image_url ?>'><img class='single_img' src='<?php echo  $full_image_url ?>' /></a>
-               <?php
-             }
-           }
-
-           ?>
+              ?>
 
 
-         </section>
+            </section>
 
-
-
+            <?php
+          } ?>
+         
     <?php
     }
    }
